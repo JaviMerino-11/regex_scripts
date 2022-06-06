7fe6bba652a6bd06a613eba91f70149b5790026f
JaviMerino-11 <javier.merinochica.practicas@telefonica.com>
2022-06-06 12:21:12 +0200

    [[feat]] Using only a huge string

--- people/People.py
+++ people/People.py
@@ -3,18 +3,19 @@ import re
 
 class People(object):
     def __init__(self):
-        self.name = input('Introduce the name: ')
-        self.first_surname = input('Introduce the first surname: ')
-        self.last_surname = input('Introduce the last surname: ')
+        self.name: str = input('Introduce the name: ')
+        self.first_surname: str = input('Introduce the first surname: ')
+        self.last_surname: str = input('Introduce the last surname: ')
+        self.full_name = self.name + ' ' + self.first_surname + ' ' + self.last_surname
 
-    def show_profile(self):
-        search_name = '^\D[A-Z\sa-z]+$'
-        search_first_surname = '^\D[A-Z\sa-z]+$'
-        search_last_surname = '^\D([A-Z\sa-z]+)${0,1}'
+    def show_personal_data(self):
+        profile_pattern = re.compile(
+            "(?P<name>([A-ZÀ-ÿ][a-z\s]*)+) (?P<first_surname>([A-ZÀ-ÿ][a-z\s]*)+) (?P<last_surname>([A-ZÀ-ÿ][a-z\s]*)?)")
 
-        if re.search(search_name, self.name) is not None and re.search(search_first_surname,
-                                                                       self.first_surname) is not None or re.search(
-            search_last_surname, self.last_surname) is '':
-            print('\nName: %s\nSurname: %s %s' % (self.name, self.first_surname, self.last_surname))
+        match_name = re.match(profile_pattern, self.full_name)
+
+        if match_name:
+            print('Name: %s\nSurname: %s %s' % (
+                match_name.group('name'), match_name.group('first_surname'), match_name.group('last_surname')))
         else:
-            print('\nFormate not allowed')
+            print('Formate Not Allowed. Check conditions')
--- regex_runner.py
+++ regex_runner.py
@@ -3,7 +3,7 @@ from people.People import People
 
 def main() -> None:
     person = People()
-    person.show_profile()
+    person.show_personal_data()
 
 
 if __name__ == '__main__':
